92b42a371335a614566d3e40ebc6ff35f91c7833
Kensuke Nagae <kyanny@gmail.com>
2012-05-10 20:35:14 +0900

    Add spec

---
+++ spec/rake_confirm_spec.rb
@@ -0,0 +1,46 @@
+require 'rake'
+
+describe "Rake::Confirm" do
+  describe "confirm" do
+    # TODO: not DRY, but I don't know how to share variables with rspec DSL in/out context
+    @confirmed_tasks     = %w(db:drop db:fixtures:load db:rollback db:migrate db:schema:load db:seed db:setup)
+    let(:confirmed_tasks){ %w(db:drop db:fixtures:load db:rollback db:migrate db:schema:load db:seed db:setup) }
+
+    @not_confirmed_tasks     = %w(db:create db:migrate:status db:schema:dump db:structure:dump db:version)
+    let(:not_confirmed_tasks){ %w(db:create db:migrate:status db:schema:dump db:structure:dump db:version) }
+
+    before(:each) do
+      # stub for rake/confirm.rb
+      module Rails
+        class Railtie
+          ::Rails::Railtie.stub!(:rake_tasks)
+        end
+      end
+
+      # # inject dummy tasks
+      confirmed_tasks.each do |t|
+        Rake::Task.define_task(t)
+      end
+
+      not_confirmed_tasks.each do |t|
+        Rake::Task.define_task(t)
+      end
+
+      # delayed loading because 'task' is evaluated before stubbing
+      require 'rake-confirm'
+      load File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'rake', 'confirm', 'tasks', 'confirm.rake'))
+    end
+
+    @confirmed_tasks.each do |t|
+      it "should make #{t} with confirmation" do
+        Rake::Task[t].prerequisites.should include(:confirm)
+      end
+    end
+
+    @not_confirmed_tasks.each do |t|
+      it "should not make #{t} with confirmation" do
+        Rake::Task[t].prerequisites.should_not include(:confirm)
+      end
+    end
+  end
+end
